ce43b11a486be1e8ffd2a8f0248e0f133dda9bc0
Dinuja Wattage <dinuja.w9@gmail.com>
2024-02-06 21:38:18 -0500

    updated starter code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React, { useState } from 'react';
 import TodoList from './TodoList';
 import AddTodo from './AddTodo';
@@ -9,11 +10,23 @@ const App = () => {
     setTodos([...todos, { id: todos.length + 1, task, completed: false }]);
   };
 
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div>
       <h1>Todo List</h1>
       <AddTodo addTodo={addTodo} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </div>
   );
 };
